057821112d1acf5adcd48bbac455dcbbd80baeb3
Dmitry Kiselev <kiselev_98@list.ru>
2020-04-30 15:05:25 +0300

    Changed some parametrs

--- face_rec.py
+++ face_rec.py
@@ -4,6 +4,7 @@
 Template doc
 """
 import argparse
+import cv2
 
 
 def get_args():
@@ -19,6 +20,38 @@ def main():
     """Application entry point."""
     args = get_args()
 
+    # Load the cascade
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+    # To capture video from webcam.
+    cap = cv2.VideoCapture(0)
+    cap.set(3, 1920 / 2)
+    cap.set(4, 1080 / 2)
+    # To use a video file as input
+    # cap = cv2.VideoCapture('filename.mp4')
+
+    while True:
+        # Read the frame
+        _, img = cap.read()
+        # Convert to grayscale
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # Detect the faces
+        # Draw the rectangle around each face
+        faces = face_cascade.detectMultiScale(gray, 1.3, 4)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+        # Display
+        cv2.imshow('img', img)
+
+        # Stop if escape key is pressed
+        k = cv2.waitKey(30) & 0xff
+        if k == 27:
+            break
+
+    # Release the VideoCapture object
+    cap.release()
 
 if __name__ == '__main__':
     main()
