dd9ce0b369c463bf759dff10226a292d0cceabae
Dmitry Kiselev <kiselev_98@list.ru>
2020-05-01 17:06:40 +0300

    Added evaluate.py and predict.py

---
+++ evaluate.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*
+"""
+Template doc
+"""
+
+import argparse
+import csv
+
+import pandas as pd
+
+from sklearn.metrics import recall_score, precision_score, accuracy_score
+from torchvision.datasets import ImageFolder
+from pathlib import Path
+
+
+def get_args():
+    """Arguments parser."""
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('--data_dir',
+                        help='Direction of data to evaluate')
+    parser.add_argument('--save_path',
+                        help='Path for saving created csv')
+    parser.add_argument('--true_path',
+                        help='Path of csv with true values')
+    parser.add_argument('--pred_path',
+                        help='Path of csv with predicted values')
+    return parser.parse_args()
+
+
+def csv_maker(data_dir, path):
+    """
+
+    Parameters
+    ----------
+    data_dir: direction
+        Direction of data to create true.csv (csv with true values)
+    path:
+        Path for created csv
+
+    Returns
+    -------
+
+    """
+    pred_dataset = ImageFolder(data_dir)
+    with open(path, "w", newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(['File', 'Class'])
+        for i in range(len(pred_dataset)):
+            writer.writerow([Path(pred_dataset.samples[i][0]).name,
+                            pred_dataset.samples[i][1]])
+
+
+def evaluate(true_path, pred_path):
+    """
+
+    Parameters
+    ----------
+    true_path: path
+        Path of csv with true values
+    pred_path: path
+        Path for csv created with predict.py
+
+    Returns
+    -------
+
+    """
+    true = pd.read_csv(true_path)
+    pred = pd.read_csv(pred_path)
+    y_true = true['Class'].to_list()
+    y_pred = pred['Class'].to_list()
+    for i in range(len(y_true)):
+        if y_pred[i] == 'Normal':
+            y_pred[i] = 0
+        else:
+            y_pred[i] = 1
+    print('Precision: {:4f} Recall: {:4f} Accuracy: {:4f}'.format(
+        precision_score(y_true, y_pred),
+        recall_score(y_true, y_pred),
+        accuracy_score(y_true, y_pred)
+    ))
+
+
+def main():
+    """Application entry point."""
+    args = get_args()
+    csv_maker(args.data_dir, args.save_path)
+    evaluate(args.true_path, args.pred_path)
+
+
+if __name__ == '__main__':
+    main()
---
+++ predict.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*
+"""
+Template doc
+"""
+import argparse
+import torch
+import torchvision
+import csv
+
+from pathlib import Path
+from torchvision import transforms
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+
+
+def get_args():
+    """Arguments parser."""
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('--model_path',
+                        help='Path of saved model')
+    parser.add_argument('--data_dir',
+                        help='Direction of data to predict')
+    parser.add_argument('--csv_path',
+                        help='Path for saving csv')
+    return parser.parse_args()
+
+
+def csv_writer(data, path):
+    """
+
+    Parameters
+    ----------
+    data:
+        List with data for writing in the csv
+    path:
+        Path for saving created csv
+    Returns
+    -------
+
+    """
+    with open(path, "w", newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(['File', 'Class'])
+        for i in range(len(data)):
+            writer.writerow(data[i].split(','))
+
+
+def predict(model_path, data_dir):
+    """
+
+    Parameters
+    ----------
+    model_path: path
+        Direction to the saved model
+    data_dir:
+        Direction to data for predicition
+    Returns
+    -------
+
+    """
+    data_transforms = transforms.Compose(
+        [transforms.Resize((224, 224)),
+         transforms.ToTensor(),
+         transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))])
+
+    pred_dataset = ImageFolder(data_dir, data_transforms)
+    pred_loader = DataLoader(pred_dataset, batch_size=64, shuffle=False)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = torchvision.models.resnet18(pretrained=False, num_classes=2)
+    model.to(device)
+    checkpoint = torch.load(model_path)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    model.eval()
+
+    predictions = []
+    for inputs, labels in pred_loader:
+        inputs = inputs.to(device)
+        outputs = model(inputs)
+        _, preds = torch.max(outputs, 1)
+        preds = preds.tolist()
+        for i in range(len(preds)):
+            predictions.append(preds[i])
+    for i in range(len(predictions)):
+        predictions[i] = str(Path(pred_dataset.samples[i][0]).name) + ',' + (
+            'Dmitry' if predictions[i] == 0 else 'unknown_person')
+    return predictions
+
+
+def main():
+    """Application entry point."""
+    args = get_args()
+    pred = predict(args.model_path, args.data_dir)
+    csv_writer(pred, args.csv_path)
+
+
+if __name__ == '__main__':
+    main()
+
